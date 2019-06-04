0bf412229e451583dbc08a525010e274bfd3f877
Maple <1105600834@qq.com>
2019-06-04 18:20:13 +0800

    Upload
    A Pin Game

---
+++ Assets/Script/GM.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour {
+    private bool isGameOver = false;
+
+
+    public Spawner spawner;
+    public Rotator rotator;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void GameOver()
+    {
+        if (!isGameOver)
+        {
+            isGameOver = true;
+            spawner.enabled = false;
+            rotator.enabled = false;
+            GetComponent<Animator>().SetTrigger("GameOver");
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) Application.Quit();
+
+    }
+}
---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject pinFrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnPin();
+        }
+    }
+    void SpawnPin()
+    {
+        Instantiate(pinFrefab, transform.position, transform.rotation);
+    }
+}
